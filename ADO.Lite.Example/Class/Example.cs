2d61e3c20740ec0813651a31543c18ead93437f0
fiado07 <32851143+fiado07@users.noreply.github.com>
2018-03-06 14:50:50 +0200

    Add files via upload

---
+++ ADO.Lite.Example/Class/Example.cs
@@ -0,0 +1,36 @@
+﻿using ADO.Lite.Contracts;
+using ADO.Lite.SqlQuery;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.Lite.Example.Class
+{
+    public class Example
+    {
+        
+        public Example()
+        {
+
+            // initialize connecton
+            BuildQuery.DbConnection = new SqlServerContext();
+
+
+        }
+
+
+        public void CheckAny()
+        {
+
+            bool resultado = BuildQuery.CheckAny<Aluno>((x) => x.nota == "10" && x.alunoID == 4);
+
+
+        }
+
+
+
+    }
+}
---
+++ ADO.Lite.Example/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADO.Lite;
+using ADO.Lite.Contracts;
+using System.Data.SqlClient;
+using System.Data;
+using ADO.Lite.SqlQuery;
+using ADO.Lite.Example.Class;
+using ADO.Lite.Enums;
+
+namespace ADO.Lite.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         
+            IDbConnectionSql sqlConnnection = new SqlServerContext();
+            BuildQuery.DbConnection = sqlConnnection;
+
+                     
+
+            // arrange
+            string sql = string.Empty;
+            int totalBefor = 0;
+            int totalAfter = 0;
+
+            Aluno aluno = new Aluno { curso = "20", Nome = "Fia" };
+
+
+            // act
+            totalBefor = BuildQuery.Count(DbProvider.SqlClient, "aluno");
+
+            // Insert query execution
+            BuildQuery.Insert(aluno, new List<string> { nameof(aluno.alunoID), nameof(aluno.data) });
+
+            totalAfter = BuildQuery.Count(DbProvider.SqlClient, "aluno");
+
+            
+            //Aluno aluno = new Aluno { alunoID = 4, nota = "10" };
+
+            bool resultado = BuildQuery.CheckAny<Aluno>((x) => x.nota == "10" && x.alunoID == 4);
+
+
+            Console.WriteLine(resultado);
+            Console.Read();
+
+
+        }
+
+
+        public static void InsertByObject()
+        {
+
+            // arrange
+            string sql = string.Empty;
+            int totalBefor = 0;
+            int totalAfter = 0;
+
+            Aluno aluno = new Aluno { curso = "20", Nome = "Fia" };
+
+
+            // act
+            totalBefor = BuildQuery.Count(DbProvider.SqlClient, "aluno");
+
+            // Insert query execution
+            BuildQuery.Insert(aluno, new List<string> { nameof(aluno.data) });
+
+            totalAfter = BuildQuery.Count(DbProvider.SqlClient, "aluno");
+
+
+            // assert 
+            //   Assert.Greater(totalAfter, totalBefor);
+
+
+        }
+
+    }
+}
